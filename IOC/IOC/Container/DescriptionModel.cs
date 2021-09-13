a222d91f217192a62b7d33c65db7aebfbae1f08d
Moh F. Hassan <fahmymohammed@gmail.com>
2021-09-13 08:56:32 -0600

    Creating DescriptionModel and LifeCycleFlag
    Creating DescriptionModel and LifeCycleFlag
    DescriptionModel Class for Registered Services
    LifeCycleFlag enum to use it as Flag to Determine the Course of Action For the Service (transient or Singleton)

---
+++ IOC/IOC/Container/DescriptionModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOC.Container
+{
+    public class DescriptionModel
+    {
+        //Registered the Service
+        public Type RegisterType { get; set; }
+
+        //Registered Service Implementation
+        public Type RegisterImplementationType { get; set; }
+        
+        //Flag to Determine the LifeCycle
+        public LifeCycleFlag LifeCycleFlag { get; set; }
+    }
+}
---
+++ IOC/IOC/Container/LifeCycleFlag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOC.Container
+{
+    public enum LifeCycleFlag
+    {
+        //Flag for Multiple Instance Across the Application
+        Transient,
+        //Flag for Single Instance Across the Application
+        Singleton
+    }
+}
