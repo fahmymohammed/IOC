8fd8328ccdb5fdc4b5a98e3491ab3f7c174bcb5a
Moh F. Hassan <fahmymohammed@gmail.com>
2021-09-13 08:42:06 -0600

    Creating Interface and the Class
    Creating Interface and the Class by
    adding ICalculator - interface
    adding Calculator Class Implementing Interface ICalculator

---
+++ IOC/IOC/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOC
+{
+    
+    public class Calculator : ICalculator
+    {
+        int numberOne = new Random().Next(0, 10);
+        int numberTwo = new Random().Next(0, 10); 
+        public void Sum() => Console.WriteLine( numberOne+ numberTwo);
+    }
+}
